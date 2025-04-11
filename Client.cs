bd531808fa52ee5911902887bf0604cf627feb8a
anton <anton@ORDI_ANTONINC>
2025-04-11 12:32:38 +0200

    nouvelle class

---
+++ Client.cs
@@ -0,0 +1,18 @@
+namespace projetcamion
+{
+    public class Client : Personne
+    {
+
+        public Client(int nss, string nom, string prenom, DateTime naissance, string adresse, string mail, int numero) : base(nss, nom, prenom, naissance, adresse, mail, numero)
+        {
+            
+        }
+        public override string ToString()
+        {
+            return $"Client: {nom} {prenom}, NSS: {nss}, Naissance: {naissance.ToShortDateString()}, Adresse: {adresse}, Mail: {mail}, Numero: {numero}";
+        }
+
+    }
+
+
+    }
