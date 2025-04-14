fbb683cd06179d0b32d060ee304dae80e699c422
Timothechbt <chabottimothe@gmail.com>
2025-04-14 11:42:52 +0200

    Suppression classe Poste et modification de Salarie et Chauffeur

--- Chauffeur.cs
+++ Chauffeur.cs
@@ -1,10 +1,10 @@
 namespace projetcamion
 {
-    public class Chauffeur : Poste
+    public class Chauffeur : Salarie
     {
         bool chauffeurDisponible;
 
-        public Chauffeur(bool chauffeurDisponible,string titre):base(titre){
+        public Chauffeur(bool chauffeurDisponible,string titre,DateTime dateEntree,string poste,float salaire,int nss, string nom, string prenom, DateTime naissance, string adresse, string mail, int numero):base(dateEntree, poste, salaire,nss,nom,prenom,naissance,adresse,mail, numero){
             this.chauffeurDisponible=chauffeurDisponible;
         }
 
--- Salarie.cs
+++ Salarie.cs
@@ -3,16 +3,16 @@ namespace projetcamion
     public class Salarie : Personne
     {
         DateTime dateEntree;
-        Poste poste;
+        string poste;
         float salaire;
 
-        public Salarie(DateTime dateEntree,Poste poste,float salaire,int nss, string nom, string prenom, DateTime naissance, string adresse, string mail, int numero):base(nss,nom,prenom,naissance,adresse,mail, numero){
+        public Salarie(DateTime dateEntree,string poste,float salaire,int nss, string nom, string prenom, DateTime naissance, string adresse, string mail, int numero):base(nss,nom,prenom,naissance,adresse,mail, numero){
             this.dateEntree=dateEntree;
             this.poste=poste;
             this.salaire=salaire;
         }
 
-        public Poste Poste{
+        public string Poste{
             get{return this.poste;}
             set{this.poste=value;}
         }
